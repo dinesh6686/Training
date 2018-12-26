03991c7c23460b6775bd75c8755d2dd0bc8ba468
Dinesh <dinesh6686@gmail.com>
2018-12-26 12:02:49 +0530

    JS assignment

--- Javascript/inheritance.js
+++ Javascript/inheritance.js
@@ -1,4 +1,17 @@
 var readline = require('readline-sync');
+var jsonObj = [{
+  "id": 3,
+  "name": "user1"
+}, {
+  "id": 6,
+  "name": "user2",
+}, {
+  "id": 4,
+  "name": "user3"
+}, {
+  "id": 1,
+  "name": "user4"
+}];
 
 function ArraySort(array) {
   this.originalArray = array;
@@ -10,6 +23,7 @@ function ArraySort(array) {
     function getSortedArray() {
       return sortedArray;
     }
+
   }
   return sortArray();
 }
@@ -19,8 +33,14 @@ function inputArray() {
   var array = JSON.parse("[" + inputStr + "]");
   console.log("sortedArray: " + ArraySort(array));
 }
-inputArray();
 
-function sortObjectArray() {
-
+function sortObjectArray(json) {
+  json.sort(function(a, b) {
+    return a.id - b.id; //sort the object on id property
+  });
+  console.log("Sorted JSON object:");
+  console.log(json);
 }
+
+inputArray();
+sortObjectArray(jsonObj);
