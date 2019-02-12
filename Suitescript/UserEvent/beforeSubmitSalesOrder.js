a8ae31560ebdc0f8ab4b8446cefeee944859ae17
Dinesh <dinesh6686@gmail.com>
2019-02-12 12:47:01 +0530

    Suitescripts

--- Suitescript/UserEvent/beforeSubmitSalesOrder.js
+++ Suitescript/UserEvent/beforeSubmitSalesOrder.js
@@ -1,7 +1,14 @@
 function beforeSubmitSalesOrder(type){
 
-     var newId = nlapiGetRecordId();
-     var newType = nlapiGetRecordType();
-     nlapiLogExecution('DEBUG','<Before Submit Script> type:'+type+', RecordType: '+newType+', Id:'+newId);
+  var company=nlapiGetFieldValue('companyname');
+  var email=nlapiGetFieldValue('email');
+  var phone=nlapiGetFieldValue('phone');
+  var newrec = nlapiCreateRecord('customrecord1199');
+  newrec.setFieldValue('name',company);
+  newrec.setFieldValue('custrecord9885653',email);
+  newrec.setFieldValue('custrecord9885654',phone);
+  var newId = nlapiSubmitRecord(newrec);
 
+  nlapiLogExecution('DEBUG','<Before Submit Script> type:'+type+', RecordType: '+newType+', Id:'+newId);
+  nlapiLogExecution('DEBUG','id'+newId);
 }
