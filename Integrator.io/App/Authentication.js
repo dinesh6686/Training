c705a20193542a0103070b786703776fa63b4a65
Dinesh Venkat <dinesh6686@gmail.com>
2020-08-19 14:15:58 +0530

    removing tokens from files

--- Integrator.io/App/Authentication.js
+++ Integrator.io/App/Authentication.js
@@ -1,4 +1,4 @@
-var accessToken ='87a9bf042e924f50b4a81238b0fa2256';
+var accessToken ='*****************';
 var url = 'https://api.integrator.io/v1';
 
 function setOptions(varUri='',varBody={},meth=''){
